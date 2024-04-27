70495ada562b0252e61090a3775134a17112367d
SpencerSzabados <>
2024-04-27 23:46:20 +0000

    Fixed fp16 paramter

--- train_autoencoder.sh
+++ train_autoencoder.sh
@@ -13,4 +13,4 @@ mpiexec --use-hwthread-cpus --oversubscribe -n $NGPU python train_autoencoder.py
     --workdir $WORK_DIR --data_dir $DATA_DIR --image_size $IMAGE_SIZE --global_batch_size $BATCH_SIZE \
     --lr $LR --ema_rate $EMA_RATE --weight_decay $WEIGHT_DECAY --lr_anneal_steps $LR_ANNEAL_STEPS \
     --log_interval $LOG_INTERVAL --save_interval $SAVE_INTERVAL --total_training_steps $TOTAL_TRAINING_STEPS \
-    --augment $AUG --ngpu 1
+    --augment $AUG --ngpu 1 -fp16 $USE_16FP
